727447c62a317852d44dab2683f0d997f74c8583
Mita Singh <78019337+mitasingh10332@users.noreply.github.com>
2022-10-12 01:27:01 +0530

    Maximum Continuous 1's

---
+++ Maximum Continuous 1's.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){                   //only 1  ans 0 are +nt.
+    int n,k;
+    cin >> n>>k;
+    vector<int> a(n);
+
+    for(auto &i: a)
+    {
+       cin>>i;
+    }
+
+    int zerocnt=0, ans=0, i=0;
+    for (int j=0;j<n;j++)
+    {
+        if (a[j]==0)
+        {
+            zerocnt++;
+        }
+
+        while(zerocnt>k)
+        {
+            if (a[i]==0)
+            {
+                zerocnt--;
+            }
+            i++;
+        }
+
+        ans=max(ans,j-i+1);
+    }
+
+    cout<<ans;
+    return 0;
+
+}
