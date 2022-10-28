af922b9a47507d27a27e0efb62e2c8b0707caed2
Mita Singh <78019337+mitasingh10332@users.noreply.github.com>
2022-10-28 20:05:18 +0530

    Reversing elements inside 2d array

---
+++ InterviewBit/Reversing elements inside 2d array.cpp
@@ -0,0 +1,11 @@
+vector<vector<int> > performOps(vector<vector<int> > &A) {
+    vector<vector<int> > B;
+    B.resize(A.size());
+    for (int i = 0; i < A.size(); i++) {
+        B[i].resize(A[i].size());
+        for (int j = 0; j < A[i].size(); j++) {
+            B[i][A[i].size() - 1 - j] = A[i][j];
+        }
+    }
+    return B;
+}
